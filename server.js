fd1bd6d4b08547c00e1448afdf7ee4c9585cec64
owaisbukhari00 <owaisbukhari@outlook.com>
2023-09-24 16:36:49 +0500

    added cors to app

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const PORT = 8000;
 
@@ -19,6 +20,8 @@ const rappers = {
     'age' : 0
   }
 };
+
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
